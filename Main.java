9d29a303a8609bd3aa1fabc81875dd12fb7e0e7a
Xenia Plotnikova <lisaplotnikova@yandex.ru>
2020-04-06 11:30:22 +0300

    Adding Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long amount = 1500000;
+        long miles_bonus = amount / 2000;
+        System.out.println(miles_bonus);
+    }
+}
+
